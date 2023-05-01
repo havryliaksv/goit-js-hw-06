3c5873b2bf17f58c80866311f9e2db6117ae1aaf
havryliaksv <gavriliaks@gmail.com>
2023-05-01 21:08:29 +0300

    task 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,25 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const makeMarkupIngredientCard = (ingredient) => {
+  const ingredientRef = document.createElement("li");
+  ingredientRef.textContent = ingredient;
+  ingredientRef.classList.add("item");
+  return ingredientRef;
+};
+
+const addMarkupIngredientList = (ingredientsListSelector, ingredientsArray) => {
+  const listIngredientsRef = document.querySelector(ingredientsListSelector);
+  return listIngredientsRef
+    ? listIngredientsRef.append(...ingredientsArray)
+    : console.log(`Element "${ingredientsListSelector}" not found`);
+};
+
+const markupIngredientsList = ingredients.map(makeMarkupIngredientCard);
+addMarkupIngredientList("#ingredients", markupIngredientsList);
