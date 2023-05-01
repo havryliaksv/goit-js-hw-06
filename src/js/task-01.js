58dbc25d3680d8379ac98f0d91ec86eaeddfe930
havryliaksv <gavriliaks@gmail.com>
2023-05-01 17:31:11 +0300

    task 1

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -4,3 +4,12 @@ const getCountListItem = (listSelector) =>
     : document.querySelector(listSelector).children.length;
 
 console.log(`Number of categories: ${getCountListItem("#categories")}`);
+
+const getDescriptionListCategories = () => {
+  const listCategories = document.querySelector("#categories").children;
+  [...listCategories].forEach((item) => {
+    console.log(`Category: ${item.querySelector("h2").textContent}`);
+    console.log(`Elements: ${item.querySelectorAll("ul li").length}`);
+  });
+};
+getDescriptionListCategories();
