bf081db8502577450f3316a98cc6ba9ded7f192f
havryliaksv <gavriliaks@gmail.com>
2023-05-03 21:06:41 +0300

    task 10

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -3,3 +3,38 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const refs = {
+  input: document.querySelector("#controls > input"),
+  btnCreate: document.querySelector("[data-create]"),
+  btnDestroy: document.querySelector("[data-destroy]"),
+  divBoxes: document.querySelector("#boxes"),
+};
+
+function createBoxes(amount) {
+  let width = 30;
+  let height = 30;
+  let rowMarkup = ``;
+  for (let i = 0; i < amount; i += 1) {
+    rowMarkup += `<div style="width: ${width}px; height: ${height}px; background-color: ${getRandomHexColor()}"></div>`;
+    width += 10;
+    height += 10;
+  }
+  refs.divBoxes.innerHTML = rowMarkup;
+  console.log(`Insert ${amount} elements into "#boxes"`);
+}
+
+refs.btnCreate.addEventListener("click", onClickBtnCreate);
+
+function onClickBtnCreate() {
+  const amount = refs.input.value ? refs.input.value : 0;
+  createBoxes(amount);
+}
+
+refs.btnDestroy.addEventListener("click", onClickBtnDestroy);
+
+function onClickBtnDestroy() {
+  const delBoxes = refs.divBoxes.children.length;
+  console.log(`Removed ${delBoxes} elements from "#boxes"`);
+  refs.divBoxes.innerHTML = "";
+}
