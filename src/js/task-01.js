ca8ba92fa25e0a3ae455f6cd8145853e9bdccf27
havryliaksv <gavriliaks@gmail.com>
2023-05-03 09:17:35 +0300

    task 1

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -9,7 +9,7 @@ const getDescriptionListCategories = () => {
   const listCategories = document.querySelector("#categories").children;
   [...listCategories].forEach((item) => {
     console.log(`Category: ${item.querySelector("h2").textContent}`);
-    console.log(`Elements: ${item.querySelectorAll("ul li").length}`);
+    console.log(`Elements: ${item.querySelectorAll("ul > li").length}`);
   });
 };
 getDescriptionListCategories();
