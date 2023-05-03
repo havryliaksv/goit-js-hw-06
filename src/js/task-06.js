eaa840e48881a9ce6e71dcf4c7fe248f2fe49ac7
havryliaksv <gavriliaks@gmail.com>
2023-05-03 14:40:36 +0300

    task 6

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,3 +1,17 @@
 const refs = {
   input: document.querySelector("#validation-input"),
 };
+
+refs.input.addEventListener("blur", onBlurInput);
+
+function onBlurInput() {
+  if (
+    refs.input.value.length < Number(refs.input.getAttribute("data-length"))
+  ) {
+    refs.input.classList.add("invalid");
+    refs.input.classList.remove("valid");
+  } else {
+    refs.input.classList.add("valid");
+    refs.input.classList.remove("invalid");
+  }
+}
