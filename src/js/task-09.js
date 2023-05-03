e3611726c588c233b844f117a7d9278fa5719013
havryliaksv <gavriliaks@gmail.com>
2023-05-03 18:48:18 +0300

    task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,17 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const refs = {
+  refChangeColor: document.body,
+  btnChangeColor: document.querySelector(".change-color"),
+  textColorValue: document.querySelector(".color"),
+};
+
+refs.btnChangeColor.addEventListener("click", onClickBtnChangeColor);
+
+function onClickBtnChangeColor() {
+  const color = getRandomHexColor();
+  refs.refChangeColor.style.backgroundColor = color;
+  refs.textColorValue.textContent = color;
+}
